b59892659236339933ff3edb0bace5e2d4dc157d
randisumitro <136390141+randisumitro@users.noreply.github.com>
2023-06-13 05:00:19 +0700

    Add files via upload

---
+++ 1. HTML5 Dasar/js/script.js
@@ -0,0 +1,11 @@
+// toggle class active
+const navbarNav = document.querySelector
+    ('.navbar-nav');
+//  ketika hamburger menu di klik
+document.querySelector('#hamburger-menu')
+    .onclick = () => {
+        navbarNav.classList.toggle('active');
+    };
+
+// Klik di luar sidebar untuk 
+const
